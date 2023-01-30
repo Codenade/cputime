26f36092009b3155555cccf8d6b3df96520480e8
Dorian <amadeus.dorian04@gmail.com>
2023-01-30 16:17:59 +0100

    almost done

--- toptasks.py
+++ toptasks.py
@@ -1,14 +1,22 @@
-import os
+import time
 import keyboard
-import psutil
+import psutil        
+
+time_begin = time.time()
+
+def log(*o : object) -> None:
+    print(str(time.time() - time_begin) + str(o))
 
 while not keyboard.is_pressed("esc"):
-    ps = list()
-    pids = psutil.pids()
+    processes = list()
     top = list()
-    for pid in pids:
-        ps.append(psutil.Process(pid).cpu_percent())
-    ps.sort()
-    for i in range(len(ps)-1,len(ps)-1):
-        top.append(i)
-    print(top)
+    for process in psutil.process_iter():
+        processes.append(process)
+    try:
+        sorted(processes,key=lambda a: a.cpu_times().user)
+        for i in range(0,10):
+            top.append([processes[i].name(),processes[i].cpu_times().user])
+        log(top)
+        time.sleep(1)
+    except:
+        log("-")
