60e4538296375ff2ac014e8f5b2f4210f05c355d
Codenade <amadeus.dorian04@gmail.com>
2023-01-31 18:49:29 +0100

    csv export complete

--- toptasks.py
+++ toptasks.py
@@ -1,14 +1,18 @@
+
 import os
-import io
+import sys
 import math
 import time
 import keyboard
 import psutil
+import argparse
 
 time_begin = time.time()
 time_meas = time.time()
 times = dict()
 interval = 0.5
+rows = 10
+file = None
 
 def log(*o : object) -> None:
     rt = time.time() - time_begin
@@ -18,6 +22,17 @@ def log(*o : object) -> None:
         _o = o
     print(f"[{math.floor((rt % 86400) / 3600):02}:{math.floor((rt % 3600) / 60):02}:{math.floor(rt % 60):02}.{math.floor((rt*1000)%1000):003}]: {_o!s}")
 
+parser = argparse.ArgumentParser(description="track top cpu processes")
+parser.add_argument('file',type=str)
+args = parser.parse_args()
+if not args.file == "121279271498752186538765876138768162872838762736762876458176258723685637857":
+    file = open(args.file, "wt")
+else:
+    file = sys.stdout
+file.write("time, ")
+for i in range (1, rows-1):
+    file.write(f"#{i}, p{i}, ")
+file.write(f"#{rows}, p{rows}\n")
 while not keyboard.is_pressed("esc"):
     nonidle = dict()
     for process in psutil.process_iter():
@@ -32,10 +47,17 @@ while not keyboard.is_pressed("esc"):
     time_meas = time.time()
     try:
         snonidle = sorted(nonidle.items(), key=lambda o: o[1])
-        outstr = str()
-        for i in range(0,min(10,len(snonidle)-1)):
-            outstr += f"{os.linesep}{snonidle[i][1][0]:<30} {'{:2.1f}'.format(snonidle[i][1][1]):>20}%"
-        log(outstr)
+        if len(snonidle) > 0:
+            time_delta = time.time() - time_begin
+            #file.write(f"{math.floor((time_delta % 86400) / 3600):02}:{math.floor((time_delta % 3600) / 60):02}:{math.floor(time_delta % 60):02}.{math.floor((time_delta*1000)%1000):003}")
+            file.write(str(round(time_delta,3)))
+            for i in range(0,rows-1):
+                item = ["", ""]
+                if i in range(0, len(snonidle)):
+                    item = snonidle[i][1]
+                file.write(f", {item[0]}, {item[1]}")
+            file.write("\n")
     except Exception as e:
         log(e)
     time.sleep(interval)
+file.close()
